ba7e50239cb32912ef359beb710ecd72eb026c92
Michael Holmes <michaelholmes618@gmail.com>
2021-04-23 17:36:13 -0500

    create handleClick function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+function handleClick() {
+  console.log("Clicked");
+}
+
 function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
